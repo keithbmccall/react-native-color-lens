af10ca9f2bdf2fc642dcf2b08e2adf2192dd54eb
keithmccall <keithcodes@gmail.com>
2020-05-17 19:42:02 -0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { NativeModules} from 'react-native';
 const { ColorLens: getPaletteFromImage} = NativeModules;
 
-export const getSwatches = (options, path, callback) => {
+export const getPalette = (options, path, callback) => {
   const localIdentifier = path.substring(5)
   getPaletteFromImage(localIdentifier, (err,res)=>{
     if (err){
