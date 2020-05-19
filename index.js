f8fcb6aaa03a18268f796e515ac2c11440d0859a
keithmccall <keithcodes@gmail.com>
2020-05-18 21:27:35 -0700

    1.0.4

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import { NativeModules} from 'react-native';
-const { ColorLens: getPaletteFromImage} = NativeModules;
+import { NativeModules } from "react-native";
+const { ColorLens: { getPaletteFromImage } } = NativeModules
 
-export const getPalette = ( path, callback) => {
-  const localIdentifier = path.substring(5)
-  getPaletteFromImage(localIdentifier, (err,res)=>{
-    if (err){
-      callback(err)
+export const getPalette = (path, callback) => {
+  const localIdentifier = path.substring(5);
+  getPaletteFromImage(localIdentifier, (err, res) => {
+    if (err) {
+      callback(err);
     } else {
-      callback(null,res)
+      callback(null, res);
     }
-  })
-
-}
+  });
+};
