9bd97ce5c0fe1dde9aeb8927072c035c1b9ab6a5
keithmccall <keithcodes@gmail.com>
2020-07-12 20:37:57 -0700

    2.0.0

--- ios/ColorLens.h
+++ ios/ColorLens.h
@@ -1,3 +1,10 @@
+//  ColorLens.h
+//  ColorLens
+//
+//  Created by Keith McCall on 7/12/20.
+//  Copyright © 2020 acid. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
 #import <GLKit/GLKit.h>
 #import "RCTBridgeModule.h"
